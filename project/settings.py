eb3f247797bf4bbc1267c8cd5503282a899f30c2
Allysson Carlos <allyssoncarlos.ac21@gmail.com>
2024-07-29 01:43:16 -0300

    Remove a pasta certo para produçao

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'P]g82*V%l]*7B,+^Kl?,A~VVdwE6tpHt65{BJ]A4$+999NmAbcC(}Y|Yb9P'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
