fe6305ca08bd8d7d26847499153fae6f1167b3e2
Allysson Carlos <allyssoncarlos.ac21@gmail.com>
2024-07-29 02:03:38 -0300

    Remove a pasta certo para produçao

--- project/settings.py
+++ project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'Pg82*V%l*7B+Kl?AVVdwE6tpHt65BJA4$+999NmAbcC(YYb9P'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.flexstreamtv.com.br']
 
 
 # Application definition
